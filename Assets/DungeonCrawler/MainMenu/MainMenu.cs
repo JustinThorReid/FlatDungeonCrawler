b08bd64fc80825171f810e9036d24481acae5482
Justin Reid <phatness0@gmail.com>
2020-08-05 01:01:25 -0400

    Add main menu screen

---
+++ Assets/DungeonCrawler/MainMenu/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public TMP_InputField nameInput;
+    private const string charName = "CharacterName";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string name = PlayerPrefs.GetString(charName);
+
+        if(name != null) {
+            nameInput.text = name;
+        }
+    }
+
+    public void StartOffline() {
+        if(string.IsNullOrEmpty(nameInput.text))
+            return;
+
+        PlayerPrefs.SetString(charName, nameInput.text);
+    }
+
+    public void StartOnline() {
+        if(string.IsNullOrEmpty(nameInput.text))
+            return;
+
+        PlayerPrefs.SetString(charName, nameInput.text);
+    }
+}
